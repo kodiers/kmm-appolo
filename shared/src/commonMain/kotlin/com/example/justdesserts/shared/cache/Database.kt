403dcd7f2443a5c7f9261be024ef253c3a18ad7f
Victor Yamchinov <kodiers@gmail.com>
2022-03-11 02:33:51 +0300

    added schema and database queries

---
+++ shared/src/commonMain/kotlin/com/example/justdesserts/shared/cache/Database.kt
@@ -0,0 +1,61 @@
+package com.example.justdesserts.shared.cache
+
+class Database(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = JustDesserts(databaseDriverFactory.createDriver())
+    private val dbQuery = database.justDessertsQueries
+
+    internal fun clearDatabase() {
+        dbQuery.transaction {
+            dbQuery.removeAllDesserts()
+        }
+    }
+
+    internal fun getDesserts(): List<Dessert> {
+        return dbQuery.selectAllDesserts().executeAsList()
+    }
+
+    internal fun getDessertById(dessertId: String): Dessert? {
+        return dbQuery.selectDessertById(dessertId).executeAsOneOrNull()
+    }
+
+    internal fun saveDessert(dessert: Dessert) {
+        dbQuery.transaction {
+            insertDessert(dessert)
+        }
+    }
+
+    internal fun updateDessert(dessert: Dessert) {
+        dbQuery.transaction {
+            updateDessertById(dessert)
+        }
+    }
+
+    internal fun deleteDessert(dessertId: String) {
+        dbQuery.transaction {
+            removeDessert(dessertId)
+        }
+    }
+
+    private fun removeDessert(dessertId: String) {
+        dbQuery.removeDessertById(dessertId)
+    }
+
+    private fun updateDessertById(dessert: Dessert) {
+        dbQuery.updateDessertById(
+            name = dessert.name,
+            description = dessert.description,
+            imageUrl = dessert.imageUrl,
+            id = dessert.id
+        )
+    }
+
+    private fun insertDessert(dessert: Dessert) {
+        dbQuery.inserDessert(
+            dessert.id,
+            dessert.userId,
+            dessert.name,
+            dessert.description,
+            dessert.imageUrl
+        )
+    }
+}
