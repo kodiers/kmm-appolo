ea5e1a154b315d23061206098ecb2fab093eac8d
Victor Yamchinov <kodiers@gmail.com>
2022-03-16 02:26:49 +0300

    added repository

---
+++ shared/src/commonMain/kotlin/com/example/justdesserts/shared/repository/BaseRepository.kt
@@ -0,0 +1,11 @@
+package com.example.justdesserts.shared.repository
+
+import com.example.justdesserts.shared.ApolloProvider
+import com.example.justdesserts.shared.cache.Database
+
+open class BaseRepository(apolloProvider: ApolloProvider) {
+    val apolloClient = apolloProvider.apolloClient
+    val database: Database = apolloProvider.database
+
+
+}
